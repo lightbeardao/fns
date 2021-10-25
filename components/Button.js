b458973eac03d9bc0f7e732a166604f205934ddf
Zen Tang <zen@wayve.ai>
2021-10-24 19:15:11 -0500

    name button

--- components/Button.js
+++ components/Button.js
@@ -1,6 +1,8 @@
 
-export default ({ children, ...props }) => (
+const Button = ({ children, ...props }) => (
   <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow m-1" {...props}>
     {children}
   </button>
 )
+
+export default Button;
