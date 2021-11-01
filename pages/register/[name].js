1409ff1982f82fcb59f881976e91beb208f9814a
Zen Tang <zen@wayve.ai>
2021-10-31 19:43:38 -0500

    add metadata

--- pages/register/[name].js
+++ pages/register/[name].js
@@ -2,8 +2,51 @@ import Head from 'next/head'
 import BackgroundGradient from '../../components/background/Gradient'
 import Header from '../../components/background/Header'
 import Button from '../../components/Button'
+import DivButton from '../../components/DivButton'
 import { useState } from 'react'
 import { useRouter } from 'next/router'
+import * as Dialog from '@radix-ui/react-dialog';
+import { styled, keyframes } from '@stitches/react'
+
+
+const overlayShow = keyframes({
+  '0%': { opacity: 0 },
+  '100%': { opacity: 0.25 },
+});
+
+const StyledOverlay = styled(Dialog.Overlay, {
+  backgroundColor: 'black',
+  opacity: 0.25,
+  position: 'fixed',
+  inset: 0,
+  '@media (prefers-reduced-motion: no-preference)': {
+    animation: `${overlayShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
+  },
+});
+
+const contentShow = keyframes({
+  '0%': { opacity: 0, transform: 'translate(-50%, -48%) scale(.96)' },
+  '100%': { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
+});
+
+const StyledContent = styled(Dialog.Content, {
+  backgroundColor: 'white',
+  borderRadius: 6,
+  boxShadow: 'hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px',
+  position: 'fixed',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '90vw',
+  maxWidth: '450px',
+  maxHeight: '85vh',
+  padding: 25,
+  '@media (prefers-reduced-motion: no-preference)': {
+    animation: `${contentShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
+    willChange: 'transform',
+  },
+  '&:focus': { outline: 'none' },
+});
 
 let prettyName = {
   profilePic: 'Profile Picture',
@@ -22,7 +65,7 @@ const Metadata = ({ m, remove }) => {
     <div key={m.type} className="flex flex-col lg:flex-row lg:items-center w-full bg-white rounded-2xl transition shadow-slight hover:shadow-lg backdrop-filter backdrop-blur-3xl p-8 lg:p-10 mt-4">
       <div className="flex items-center mr-auto">
         <img className="h-14 w-14 mr-6 lg:mr-8" src="/img/avatars/1.png" alt="Project Avatar of 8a71da08-6c11-47ab-a25a-24f4b2ebec58" />
-        <div class="flex flex-col">
+        <div className="flex flex-col">
           <h4 className="font-medium text-xl">{prettyName[type] || type}</h4>
           <p className="text-gray-400">{uri}</p>
         </div>
@@ -34,6 +77,44 @@ const Metadata = ({ m, remove }) => {
   )
 }
 
+const MetadataDialog = ({ preset }) => {
+  let { description, fields, name, onSet } = preset
+  const [text, setText] = useState(fields.map(i => ""))
+
+
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <DivButton>{name}</DivButton>
+      </Dialog.Trigger>
+      <StyledOverlay />
+      <StyledContent>
+        <Dialog.Title className="font-semibold">{name}</Dialog.Title>
+        <Dialog.Description className="text-gray-500 font-light mt-2 mb-6">{description}</Dialog.Description>
+        {
+          fields.map(({ name, placeholder }, i) => (
+            <fieldset className="flex gap-4 items-center" key={i}>
+              <label className="font-light text-gray-300 uppercase">{name}</label>
+              <input className="appearance-none border-2 border-blue-50 rounded p-2 text-green-300 font-semibold leading-tight focus:outline-none focus:shadow-outline w-full placeholder-opacity-25" type="text" placeholder={placeholder} value={text[i]} onChange={e => {
+                let c = [...text];
+                c[i] = e.target.value;
+                setText(c)
+              }} />
+            </fieldset>
+          ))
+        }
+        <Dialog.Close className="flex mt-4 justify-end w-full">
+          <DivButton onClick={() => {
+            onSet(text)
+          }}>
+            Save changes
+          </DivButton>
+        </Dialog.Close>
+      </StyledContent>
+    </Dialog.Root>
+  );
+}
+
 export default function Home({ data }) {
   const router = useRouter()
   const { name } = router.query
@@ -42,6 +123,39 @@ export default function Home({ data }) {
   const addMetadata = (type, data) => {
     setMeta([...metadata, { type, ...data }])
   }
+
+  let presets = [
+    {
+      name: 'Twitter',
+      description: "Public Twitter URL",
+      fields: [
+        { name: 'url', placeholder: 'e.g. https://twitter.com/xLootProject' }
+      ],
+      onSet: ([uri]) => {
+        addMetadata('twitterProfile', { uri })
+      }
+    },
+    {
+      name: 'Website',
+      description: "Set the homepage for this DID",
+      fields: [
+        { name: 'url', placeholder: 'e.g. https://google.com' }
+      ],
+      onSet: ([uri]) => {
+        addMetadata('website', { uri })
+      }
+    },
+    {
+      name: 'Profile Picture',
+      description: "URL of your profile pic",
+      fields: [
+        { name: 'url', placeholder: 'e.g. https://source.unsplash.com/random/50x50' }
+      ],
+      onSet: ([uri]) => {
+        addMetadata('profilePic', { uri })
+      }
+    },
+  ]
 
   return (
     <div className="min-h-screen bg-gray-100">
@@ -65,22 +179,14 @@ export default function Home({ data }) {
           <h1 className="text-gray-500 text-lg font-medium mr-4">Want to add some metadata?</h1>
         </div>
         <div className="flex justify-center gap-2 items-baseline mt-2 mb-8">
-          <Button onClick={() => {
-            addMetadata('twitterProfile', { uri: 'https://twitter.com/eoverse' })
-
-          }}>Twitter url</Button>
-          <Button onClick={() => {
-            addMetadata('website', { uri: 'https://source.unsplash.com/random/50x50' })
-
-          }}>Website</Button>
-          <Button onClick={() => {
-            addMetadata('profilePic', { uri: 'https://source.unsplash.com/random/50x50' })
-          }}>Profile Pic</Button>
+          {presets.map((preset, i) => (
+            <MetadataDialog preset={preset} key={i} />
+          ))}
         </div>
         <div className="mt-4"><div>
           <div className="min-w-full">
             {metadata.map((m, idx) => (
-              <Metadata m={m} remove={() => {
+              <Metadata m={m} key={idx} remove={() => {
                 let c = [...metadata]
                 c.splice(idx, 1)
                 setMeta(c);
